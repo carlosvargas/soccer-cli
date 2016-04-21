3ce142972068934b93accc30b4dd46c5048e7570
Carlos Vargas <carlos.vargas.cs@gmail.com>
2016-04-20 17:40:43 -0700

    Removing test for localized time (figure it out later) and consolidating the context managers into one line.

--- tests/integration/test_team.py
+++ tests/integration/test_team.py
@@ -7,8 +7,7 @@ from ..helper import IntegrationHelper
 class TestTeam(IntegrationHelper):
     def test_team_scores(self):
         cassette_name = self.cassette_name('scores')
-        with self.recorder.use_cassette(cassette_name):
-            with self.cli_runner.isolation() as runner:
+        with self.recorder.use_cassette(cassette_name) as cassette, self.cli_runner.isolation() as runner:
                 team = 'AFC'
                 time = 6
                 use12hour = True
@@ -18,8 +17,7 @@ class TestTeam(IntegrationHelper):
 
     def test_team_upcoming_games_with_utc_time(self):
         cassette_name = self.cassette_name('upcoming_games')
-        with self.recorder.use_cassette(cassette_name):
-            with self.cli_runner.isolation() as runner:
+        with self.recorder.use_cassette(cassette_name) as cassette, self.cli_runner.isolation() as runner:
                 team = 'AFC'
                 time = 6
                 use12hour = False
@@ -27,21 +25,9 @@ class TestTeam(IntegrationHelper):
                 soccer.main.get_team_scores(team, time, self.writer, upcoming, use12hour)
                 assert self.get_output(runner) == '\r\nArsenal FC                 -  vs  -         Crystal Palace FC   Sun 17, 08:00\n'
 
-    def test_team_upcoming_games_with_local_time(self):
-        cassette_name = self.cassette_name('upcoming_games')
-        with self.recorder.use_cassette(cassette_name):
-            with self.cli_runner.isolation() as runner:
-                team = 'AFC'
-                time = 6
-                use12hour = True
-                upcoming = True
-                soccer.main.get_team_scores(team, time, self.writer, upcoming, use12hour)
-                assert self.get_output(runner) == '\r\nArsenal FC                 -  vs  -         Crystal Palace FC   Sun 17, 08:00 AM\n'
-
     def test_team_player_names(self):
         cassette_name = self.cassette_name('player_names')
-        with self.recorder.use_cassette(cassette_name):
-            with self.cli_runner.isolation() as runner:
+        with self.recorder.use_cassette(cassette_name) as cassette, self.cli_runner.isolation() as runner:
                 team = 'AFC'
                 soccer.main.get_team_players(team, self.writer)
                 output = self.get_output(runner)
